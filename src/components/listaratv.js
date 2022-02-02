f6b86ed74000ed12721407f6e21a61df6e7ba7a6
vinicius <vinibessa2005@gmail.com>
2022-02-02 16:46:29 -0300

    ok

--- src/components/listaratv.js
+++ src/components/listaratv.js
@@ -1,26 +1,36 @@
 import ContComponent from "./styled"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function ComponentListar(props){
 
 const removeAtividade = () => {
     let titulo = props.info.titulo 
        props.onRemove(props.index)
-       //toast.success(' Atividade ' + titulo + ' Removida')
+       toast.success(' Atividade ' + titulo + ' Removida')
 }
 
 return(
     <ContComponent>
-                    <div>
+        
+                    <div className='ConteudoComp'>
+
+                        <div> Título </div>
                         <input
                             className= 'titulo'
                             value={props.info.titulo}
                             //onChange={onChangeAtividadeTitulo(index)}
                         />
-                        <div className = 'descricao'>{props.info.descricao}</div>
+                        <div> Descrição </div>
+                        <input 
+                            className = 'descricao' 
+                            value={props.info.descricao}
+                        />
                         <div classname = 'concluido'>{props.info.concluido}</div>
                     
                          <button className = 'Botao' onClick={removeAtividade}> Deletar </button> 
                     </div>
+            
     </ContComponent>
 )
     
--- src/components/styled.js
+++ src/components/styled.js
@@ -4,6 +4,22 @@ const ContComponent = styled.div`
 
 margin-top: 20px;
 margin-left: 100px;
+display: flex;
+flex-direction: column;
+
+.Botao{
+    margin-top: 5px;
+    color: #f5f5f5;
+    background-color: #6C698D;
+    border: none;
+    border-radius: 5px;
+    padding: 5px;
+}
+
+.titulo{
+    margin-right: 10px;
+    margin-bottom: 10px;
+}
 
 `
 
